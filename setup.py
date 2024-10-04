bd068fc1725c6bbfdaa0493d591f1ce663629fe6
J <jia.lu.angela@gmail.com>
2024-10-04 13:45:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ML',  # Name of your project
-    description='Machine learning code for Lu, Jia, et al. Decoding pattern formation rules by integrating mechanistic modeling and deep learning." bioRxiv (2024)',
+    description='Machine learning code for Lu, Jia, et al. Decoding pattern formation rules by integrating mechanistic modeling and deep learning. bioRxiv (2024)',
     author='Jia Lu',
     packages=find_packages(),  
     install_requires=['numpy', 
